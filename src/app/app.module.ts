4b1750e50b3e9f3c8f485aaf5292ad9f4f77d390
Karolina Poslek <karolina@poslek.com>
2019-04-08 11:37:03 +0200

    Created CounterService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { CounterComponent } from './counter/counter.component';
+import { CounterService } from './services/counter/counter.service';
 
 @NgModule({
   declarations: [AppComponent, MyButtonComponent, CounterComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
